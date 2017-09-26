e762544d58738c8de80ee5bb313a85506332952e
yuebo <yuebo@eappcat.com>
2017-09-26 19:14:31 +0800

    add freemaker to test

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,22 +1,96 @@
 package hello;
 
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.ScriptResult;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.WebRequest;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class Application {
+	@Autowired
+	WebClient webClient;
 
 	@RequestMapping("/")
+	@ResponseBody
 	public String home() {
 		return "Hello Openshift World, enjoy yourself with Openshift.";
 	}
 
 
+
+
+	@GetMapping("/vip")
+	public String vip(@RequestParam("url") String url, Model model) throws Exception{
+		String parseURL=parseVideo(url);
+		model.addAttribute("url",parseURL);
+
+//		URL Url=new URL(parseURL);
+//		HttpURLConnection connection=(HttpURLConnection) Url.openConnection();
+//		connection.setDoInput(true);
+//		connection.setDoOutput(true);
+//		connection.setInstanceFollowRedirects(true);
+//		connection.connect();
+//		String contentType=connection.getContentType();
+//
+//		if(contentType.toLowerCase().contains("application/x-mpegurl")){
+//			ByteArrayOutputStream outputStream=new ByteArrayOutputStream();
+//			IOUtils.copy(connection.getInputStream(),outputStream);
+//			String base64=Base64.getEncoder().encodeToString(outputStream.toByteArray());
+//			model.addAttribute("url",String.format("data:%s;base64,%s", contentType,base64));
+//			model.addAttribute("type","application/x-mpegurl");
+//
+//		}
+//		model.addAttribute("type",contentType);
+
+//		connection.disconnect();
+
+		return "vip";
+
+	}
+	@GetMapping("/vip/api")
+    @ResponseBody
+    public Map<String,String> vipapi(@RequestParam("url") String url) throws Exception{
+        String parseURL=parseVideo(url);
+        HashMap<String,String> result=new HashMap();
+        result.put("url",parseURL);
+        return result;
+
+    }
+
+	private String parseVideo(String url) throws Exception{
+		WebRequest webRequest=new WebRequest(new URL("http://api.baiyug.cn/vip_vip/index.php?url="+url));
+		webRequest.setAdditionalHeader("Referer","http://api.baiyug.cn/vip/index.php?url="+url);
+		HtmlPage p=webClient.getPage(webRequest);
+		ScriptResult result=p.executeJavaScript("basea17kdv(ykyun)");
+		String text=(String)result.getJavaScriptResult();
+		return text;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+
+	public @Bean  WebClient getWebClient(){
+		 WebClient webClient=new WebClient(BrowserVersion.CHROME);
+         webClient.getOptions().setCssEnabled(false);
+		 return webClient;
 	}
 
 }
---
+++ src/main/resources/templates/vip.ftl
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="referrer" content="no-referrer" />
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <video src="${url}" id="example-video" width="600" height="300" class="video-js vjs-default-skin" controls>
+    </video>
+</body>
+</html>
+
