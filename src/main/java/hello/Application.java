ccd9e16087ba2e7dc2fb19684396552f750d31b6
yuebo <yuebo@eappcat.com>
2017-10-02 01:06:01 +0800

    remove player

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,39 +1,23 @@
 package hello;
 
-import com.gargoylesoftware.htmlunit.BrowserVersion;
-import com.gargoylesoftware.htmlunit.ScriptResult;
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.WebRequest;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import org.apache.commons.io.IOUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.io.ByteArrayOutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Base64;
-import java.util.HashMap;
-import java.util.Map;
 
 @SpringBootApplication
 @Controller
 public class Application {
-	@Autowired
-	WebClient webClient;
-
 	@RequestMapping("/")
 	@ResponseBody
 	public String home() {
 		return "Hello Openshift World, enjoy yourself with Openshift.";
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
 	}
 
 }
