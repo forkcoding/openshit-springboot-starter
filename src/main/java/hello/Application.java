777006dfa2e15ad6061d8eb7f7fdb7fdafe0bde0
yuebo <yuebo@eappcat.com>
2017-10-02 04:44:20 +0800

    update player

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -10,13 +10,13 @@ import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import org.apache.commons.io.IOUtils;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -46,9 +46,15 @@ public class Application {
 
 	@GetMapping("/vip")
 	public String vip(@RequestParam("url") String url, @RequestParam(value = "vip",defaultValue = "2")String vip, Model model) throws Exception{
-		String parseURL=parseVideo(url,vip.equalsIgnoreCase("7"));
+		String parseURL=parseVideo(url,vip.equalsIgnoreCase("7"),model);
 		model.addAttribute("url",parseURL);
 		model.addAttribute("refer",url);
+		model.addAttribute("vip",vip.equalsIgnoreCase("7"));
+
+		if(StringUtils.isEmpty(parseURL)){
+			model.addAttribute("type","mp4");
+			return "vip";
+		}
 
 		URL Url=new URL(parseURL);
 		HttpURLConnection connection=(HttpURLConnection) Url.openConnection();
@@ -72,22 +78,23 @@ public class Application {
 
 		connection.disconnect();
 
+
 		return "vip";
 
 	}
 	@GetMapping("/vip/api")
     @ResponseBody
-    public Map<String,String> vipapi(@RequestParam("url") String url,@RequestParam(value = "vip",defaultValue = "2")String vip) throws Exception{
-        String parseURL=parseVideo(url,vip.equalsIgnoreCase("7"));
+    public Map<String,String> vipapi(@RequestParam("url") String url,@RequestParam(value = "vip",defaultValue = "2")String vip,Model model) throws Exception{
+        String parseURL=parseVideo(url,vip.equalsIgnoreCase("7"),model);
         HashMap<String,String> result=new HashMap();
         result.put("url",parseURL);
         return result;
 
     }
 
-	private String parseVideo(String url,boolean vip) throws Exception{
+	private String parseVideo(String url,boolean vip,Model model) throws Exception{
 		if(!vip){
-			return parseURL(url);
+			return parseURL(url,model);
 		}
 		WebRequest webRequest=new WebRequest(new URL("http://api.baiyug.cn/vip_vip/index.php?url="+url));
 		webRequest.setAdditionalHeader("Referer","http://api.baiyug.cn/vip/index.php?url="+url);
@@ -113,7 +120,7 @@ public class Application {
 	}
 
 
-	private String parseURL(String url) throws Exception{
+	private String parseURL(String url,Model model) throws Exception{
 		Connection test= Jsoup.connect("http://api.baiyug.cn/vip_vip/index.php?url="+url);
 		test.header("Referer","http://api.baiyug.cn/vip/index.php?url="+url);
 
@@ -130,37 +137,38 @@ public class Application {
 		String guid=preInfo.getString("guid");
 
 
+//
+//
+//		String getVkey="http://h5vv.video.qq.com/getinfo?callback=txplayerJsonpCallBack_getinfo_378876&charge=0&vid="+vid+"&defaultfmt=auto&otype=json&guid="+guid+"&platform=10901&defnpayver=1&appVer=3.2.157&sdtfrom=v1010&host=v.qq.com&ehost="+preInfo.getString("url")+"&_rnd="+_rnd+"&defn=mp4&fhdswitch=0&show1080p=1&isHLS=0&newplatform=10901&defsrc=1&_qv_rmt="+rmt+"&_qv_rmt2="+rmt2+"&_="+Math.random();
+//
+//		String getKey="http://h5vv.video.qq.com/getkey?callback=txplayerJsonpCallBack_getinfo_378876&charge=0&vid="+vid+"&filename="+vid+".mp4&format=2&otype=json&guid="+guid+"&platform=10901&defnpayver=0&appVer=3.2.157&vt=203&sdtfrom=v1010&_rnd="+_rnd+"&_qv_rmt="+rmt+"&_qv_rmt2="+rmt2+"&_="+Math.random();
+//
+//		Connection connection=Jsoup.connect(getVkey).ignoreContentType(true);
+//		Document document=connection.get();
+//		String text=document.text();
+//		String json=text.substring("txplayerJsonpCallBack_getinfo_378876".length()+1,text.length()-1);
+//
+//		JSONObject jsonObject= JSON.parseObject(json);
+//
+//		JSONObject root=jsonObject.getJSONObject("vl").getJSONArray("vi").getJSONObject(0);
+//		JSONObject ui=root.getJSONObject("ul").getJSONArray("ui").getJSONObject(1);
+//		String videoUrl=ui.getString("url");
+//		Integer vt=ui.getInteger("vt");
+//
+//
+//		Connection connection2=Jsoup.connect(getKey).ignoreContentType(true);
+//
+//		Document document2=connection2.get();
+//		String text2=document2.text();
+//		String json2=text2.substring("txplayerJsonpCallBack_getinfo_378876".length()+1,text2.length()-1);
+//
+//		String vk= JSON.parseObject(json2).getString("key");
+//		String filename= JSON.parseObject(json2).getString("filename");
 
+		model.addAttribute("videoInfo",JSON.toJSONString(preInfo));
 
-		String getVkey="http://h5vv.video.qq.com/getinfo?callback=txplayerJsonpCallBack_getinfo_378876&charge=0&vid="+vid+"&defaultfmt=auto&otype=json&guid="+guid+"&platform=10901&defnpayver=1&appVer=3.2.157&sdtfrom=v1010&host=v.qq.com&ehost="+preInfo.getString("url")+"&_rnd="+_rnd+"&defn=mp4&fhdswitch=0&show1080p=1&isHLS=0&newplatform=10901&defsrc=1&_qv_rmt="+rmt+"&_qv_rmt2="+rmt2+"&_="+Math.random();
-
-		String getKey="http://h5vv.video.qq.com/getkey?callback=txplayerJsonpCallBack_getinfo_378876&charge=0&vid="+vid+"&filename="+vid+".mp4&format=2&otype=json&guid="+guid+"&platform=10901&defnpayver=0&appVer=3.2.157&vt=203&sdtfrom=v1010&_rnd="+_rnd+"&_qv_rmt="+rmt+"&_qv_rmt2="+rmt2+"&_="+Math.random();
-
-		Connection connection=Jsoup.connect(getVkey).ignoreContentType(true);
-		Document document=connection.get();
-		String text=document.text();
-		String json=text.substring("txplayerJsonpCallBack_getinfo_378876".length()+1,text.length()-1);
-
-		JSONObject jsonObject= JSON.parseObject(json);
-
-		JSONObject root=jsonObject.getJSONObject("vl").getJSONArray("vi").getJSONObject(0);
-		JSONObject ui=root.getJSONObject("ul").getJSONArray("ui").getJSONObject(1);
-		String videoUrl=ui.getString("url");
-		Integer vt=ui.getInteger("vt");
-
-
-		Connection connection2=Jsoup.connect(getKey).ignoreContentType(true);
-
-		Document document2=connection2.get();
-		String text2=document2.text();
-		String json2=text2.substring("txplayerJsonpCallBack_getinfo_378876".length()+1,text2.length()-1);
-
-		String vk= JSON.parseObject(json2).getString("key");
-		String filename= JSON.parseObject(json2).getString("filename");
-
-
-
-		return String.format("%s%s?vkey=%s",videoUrl,filename,vk);
+//		return String.format("%s%s?vkey=%s",videoUrl,filename,vk);
+		return "";
 	}
 
 }
--- src/main/resources/templates/vip.ftl
+++ src/main/resources/templates/vip.ftl
@@ -37,17 +37,51 @@
     <#--</video>-->
     <div id="player" class="vid-wrapper" style="margin: 0 auto">
     <script type="text/javascript">
-        var letvURL = "${url}";
-        SewisePlayer.setup({
-            server: "vod",
-            type: "${type}",
-            videourl: letvURL,
-            skin: "vodWhite",
-            claritybutton: "disable",
-            title: "播放器",
-            logo: null,
-            lang: 'zh_CN'
-        },"player");
+        function load_video(url){
+            var letvURL = url;
+            SewisePlayer.setup({
+                server: "vod",
+                type: "${type}",
+                videourl: letvURL,
+                skin: "vodWhite",
+                claritybutton: "disable",
+                title: "播放器",
+                logo: null,
+                lang: 'zh_CN'
+            },"player");
+        }
+        $(document).ready(function () {
+            <#if vip>
+                load_video("${url}")
+            </#if>
+            <#if !vip>
+                var videoInfo=${videoInfo};
+
+                var vid=videoInfo.vid;
+                var _rnd=videoInfo._rnd;
+                var rmt=videoInfo._qv_rmt;
+                var rmt2=videoInfo._qv_rmt2;
+                var guid=videoInfo.guid;
+                $.ajax({
+                    url: "http://h5vv.video.qq.com/getinfo?callback=txplayerJsonpCallBack_getinfo_378876&charge=0&vid="+vid+"&defaultfmt=auto&otype=json&guid="+guid+"&platform=10901&defnpayver=1&appVer=3.2.157&sdtfrom=v1010&host=v.qq.com&ehost="+videoInfo.url+"&_rnd="+_rnd+"&defn=mp4&fhdswitch=0&show1080p=1&isHLS=0&newplatform=10901&defsrc=1&_qv_rmt="+rmt+"&_qv_rmt2="+rmt2+"&_="+Math.random(),
+                    method: "get",
+                    dataType : "jsonp",
+                    jsonpCallback:"txplayerJsonpCallBack_getinfo_378876"
+                }).success(function (data) {
+                    $.ajax({
+                        url: "http://h5vv.video.qq.com/getkey?callback=txplayerJsonpCallBack_getinfo_378876&charge=0&vid="+vid+"&filename="+vid+".mp4&format=2&otype=json&guid="+guid+"&platform=10901&defnpayver=0&appVer=3.2.157&vt=203&sdtfrom=v1010&_rnd="+_rnd+"&_qv_rmt="+rmt+"&_qv_rmt2="+rmt2+"&_="+Math.random(),
+                        method: "get",
+                        dataType : "jsonp",
+                        jsonpCallback:"txplayerJsonpCallBack_getinfo_378876"
+                    }).success(function (data2) {
+                        load_video(data.vl.vi[0].ul.ui[0].url+data2.filename+"?vkey="+data2.key);
+                    })
+                })
+
+            </#if>
+
+
+        })
     </script>
     </div>
     <a href="/qq/list?url=${refer}">返回专辑</a>
